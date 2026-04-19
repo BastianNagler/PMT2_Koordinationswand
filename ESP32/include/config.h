9101c18fb52c0e2e8c4b567fac3b124699d73878
Bastian Nagler <bastian.nagler@st.oth-regensburg.de>
2026-04-20 00:34:54 +0200

    changed pin-config for use with test-breadboard

--- ESP32/include/config.h
+++ ESP32/include/config.h
@@ -29,10 +29,10 @@ Do not use GPIOs:
 #define NUM_LEDS (NUM_FIELDS * NUM_LEDS_PER_FIELD)
 
 #define NUM_LED_DRIVERS 4 // DO NOT CHANGE
-#define LED_DRIVER_0_PIN 42 // Change to actual pin
-#define LED_DRIVER_1_PIN 41 // Change to actual pin
-#define LED_DRIVER_2_PIN 40 // Change to actual pin
-#define LED_DRIVER_3_PIN 39 // Change to actual pin
+#define LED_DRIVER_0_PIN 6 // Change to actual pin
+#define LED_DRIVER_1_PIN 7 // Change to actual pin
+#define LED_DRIVER_2_PIN 15 // Change to actual pin
+#define LED_DRIVER_3_PIN 16 // Change to actual pin
 
 #define MAX_BRIGHTNESS 255 // Adjust brightness as needed, 0-255
 
@@ -42,14 +42,14 @@ Do not use GPIOs:
 #ifdef USE_IO_EXPANDER
     #define NUM_IO_EXPANDER 4 // DO NOT CHANGE
     #define NUM_IO_PER_EXPANDER 16
-    #define I2C_SCL_PIN 9 // Change to actual pin
-    #define I2C_SDA_PIN 8 // Change to actual pin
-    #define IO_EXPANDER_0_IRQ_PIN 4 // Change to actual pins
-    #define IO_EXPANDER_1_IRQ_PIN 5
-    #define IO_EXPANDER_2_IRQ_PIN 13
-    #define IO_EXPANDER_3_IRQ_PIN 14
+    #define I2C_SCL_PIN 41 // Change to actual pin
+    #define I2C_SDA_PIN 40 // Change to actual pin
+    #define IO_EXPANDER_0_IRQ_PIN 10 // Change to actual pins
+    #define IO_EXPANDER_1_IRQ_PIN 11
+    #define IO_EXPANDER_2_IRQ_PIN 12
+    #define IO_EXPANDER_3_IRQ_PIN 13
     #define ACTIVE_LOW_TOUCH_SENSORS false // Set to true if touch sensors are active low
 #else
-    #define BUTTON_PINS {4} // manually have to increase this if more fields are added
+    #define BUTTON_PINS {10} // manually have to increase this if more fields are added
     #define ACTIVE_LOW_BUTTONS false // Set to true if buttons are active low
 #endif
